a410b17611bb565257edf4b4cce75384933ed27e
Amidou Kante <kamdiou@gmail.com>
2023-03-19 21:46:02 -0700

    add format for profile

--- hooks/usePrevious.ts
+++ hooks/usePrevious.ts
@@ -1,7 +1,7 @@
 import { useRef, useEffect } from "react";
 
 export const usePrevious: <T>(value: T) => T = (value) => {
-  const ref = useRef() as React.MutableRefObject<T>;
+  const ref = useRef() as React.MutableRefObject<typeof value>;
   useEffect(() => {
     if (ref?.current) {
       ref.current = value;
